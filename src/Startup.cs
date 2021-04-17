d330a189e2c36bd3e502259f5a400853a57ae052
Cingulara <dale.bingham@cingulara.com>
2021-04-17 13:14:01 -0400

    removing Jaeger references

--- src/Startup.cs
+++ src/Startup.cs
@@ -44,19 +44,23 @@ namespace openrmf_audit_api
                 });
             }
             
-            // Use "OpenTracing.Contrib.NetCore" to automatically generate spans for ASP.NET Core
-            services.AddSingleton<ITracer>(serviceProvider =>  
-            {                
-                ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();  
-                // use the environment variables to setup the Jaeger endpoints
-                var config = Jaeger.Configuration.FromEnv(loggerFactory);
-                var tracer = config.GetTracer();
+            if (Environment.GetEnvironmentVariable("JAEGER_AGENT_HOST") != null && 
+                !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JAEGER_AGENT_HOST"))) {
             
-                GlobalTracer.Register(tracer);  
-            
-                return tracer;  
-            });
-            services.AddOpenTracing();
+                    // Use "OpenTracing.Contrib.NetCore" to automatically generate spans for ASP.NET Core
+                    services.AddSingleton<ITracer>(serviceProvider =>  
+                    {                
+                        ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();  
+                        // use the environment variables to setup the Jaeger endpoints
+                        var config = Jaeger.Configuration.FromEnv(loggerFactory);
+                        var tracer = config.GetTracer();
+                    
+                        GlobalTracer.Register(tracer);  
+                    
+                        return tracer;  
+                    });
+                    services.AddOpenTracing();
+            }
 
             // add repositories            
             services.AddTransient<IAuditRepository, AuditRepository>();
