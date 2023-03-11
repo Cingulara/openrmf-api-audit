bb4d6c390bd05013ab51c09e4f78761f4d7a2e74
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2023-03-11 11:19:12 -0500

    fix for message error

--- src/Startup.cs
+++ src/Startup.cs
@@ -113,7 +113,7 @@ namespace openrmf_audit_api
                         c.Response.StatusCode = 401;
                         c.Response.ContentType = "text/plain";
 
-                        Console.WriteLine("openrmf-api-read JWT Error: " + c.Exception.ToString());
+                        Console.WriteLine("openrmf-api-audit JWT Error: " + c.Exception.ToString());
 
                         return c.Response.WriteAsync("The JWT validation with the server did not return correctly. Please check with your Application Administrator.");
                     }
